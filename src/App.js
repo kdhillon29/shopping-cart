2d6cce93237a0f63cbf14312b0307359bc672e3d
kanwar Dhillon <kdhillon469@gmail.com>
2022-12-16 10:52:08 +1100

    add cart context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./components/Cart";
+import NavBar from "./components/NavBar";
+import ProductList from './components/ProductList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+     <h2>Shopping Cart</h2>
+     <BrowserRouter>
+     <NavBar/>
+        <Routes>
+          <Route path="/" element={<ProductList/>} exact  />
+          
+
+          <Route path="/cart" element={<Cart/>} exact  />
+        </Routes>
+      </BrowserRouter>
+     
+      
+      
     </div>
   );
 }
